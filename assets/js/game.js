21780c712e24ac9865f83cc17451241b30638c54
Jose lopez <jlopez1313@live.com>
2021-12-17 11:34:16 -0600

    added js function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,30 @@
-window.alert("This is an alert! JavaScript is running!");
+// this creates a function named "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+//fight();
+
+var playerName=window.prompt("What is your robot's name?");
+
+//Note the lack of quatation marks around the playerName
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName);
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");

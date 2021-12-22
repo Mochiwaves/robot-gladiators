a6d49e4a6821b8eafb3c9179899ac599d30f9d34
Jose lopez <jlopez1313@live.com>
2021-12-22 04:28:04 -0600

    added localstorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -137,6 +137,24 @@ var endGame = function() {
 
     window.alert("The game has now ended. Let's see how you did!");
 
+    // check localstorage for high score, if it's not there use 0
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
+        highScore = 0;
+    }
+
+    //if player has more money than highscore, player has new high score!
+    if (playerInfo.money > highScore) {
+        localStorage.setItem("highscore", playerInfo.money);
+        localStorage.setItem("name", playerInfo.name);
+
+        alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    }
+    else {
+        alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
+ }
+ 
     //if player is still alive, player wins!
     if (playerInfo.health > 0 ) {
         window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");   
@@ -153,8 +171,8 @@ var endGame = function() {
     }
     else {
         window.alert("Thank you for playing Robot Gladiators! Come back soon!");
-    }
-};
+    };
+
 
 var shop = function(){
     
